27e02ad2d11422c6c1c5f646991db4b37f3b67fe
Sergey Masiuk <mayerror@gmail.com>
2024-01-29 16:24:03 +0300

    feat: add basis for rename function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,16 @@
+import { open, stat, mkdir, readdir, copyFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const rename = async () => {
-    // Write your code here 
+  try {
+  } catch (error) {
+    if (error.code == "EEXIST" || error.code == "ENOENT") {
+      throw Error("FS operation failed");
+    }
+  }
 };
 
 await rename();
