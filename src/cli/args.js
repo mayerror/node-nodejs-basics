13a29e1ac96cfb33c5136580112efff0a71446cc
Sergey Masiuk <mayerror@gmail.com>
2024-01-29 19:19:59 +0300

    feat: implement parseArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,25 @@
 const parseArgs = () => {
-    // Write your code here 
+  let isValue = false;
+  let str = "";
+  process.argv
+    .filter((arg) => {
+      if (isValue) {
+        isValue = false;
+        return true;
+      } else if (arg.match(/^--*/)) {
+        isValue = true;
+        return true;
+      }
+      return false;
+    })
+    .forEach((item, i) => {
+      if (i % 2 == 0) {
+        str = str.concat(`${item.slice(2)} is `);
+      } else {
+        str = str.concat(`${item}, `);
+      }
+    });
+  console.log(str.trim().slice(0, -1));
 };
 
 parseArgs();
