1f0a395da7815776e1819af0474ef2ec2c22b466
Sergey Masiuk <mayerror@gmail.com>
2024-01-29 21:46:15 +0300

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,19 @@
+import { createHash } from "node:crypto";
+import { readFile } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { stdout } from "node:process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const calculateHash = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+  const content = await readFile(filePath, { encoding: "utf8" });
+
+  let digest = createHash("sha256").update(content).digest("hex");
+
+  //logs it into console as hex using Streams API
+  stdout.write(digest);
 };
 
 await calculateHash();
