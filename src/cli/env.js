fc3b886a0fba8a50ed95a1d3d9e5ba16c1b959f7
Sergey Masiuk <mayerror@gmail.com>
2024-01-29 18:34:23 +0300

    feat: implement env function

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,13 @@
 const parseEnv = () => {
-    // Write your code here 
+  const envObj = process.env;
+
+  let str = "";
+  for (const property in envObj) {
+    if (property.match("RSS_")) {
+      str = str.concat(`${property} = ${envObj[property]}; `);
+    }
+  }
+  console.log(str.trim().slice(0, -1));
 };
 
 parseEnv();
