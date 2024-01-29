d5a76ca64f12e0ba6582fba52b8586ebc3e2e6d6
Sergey Masiuk <mayerror@gmail.com>
2024-01-29 23:26:05 +0300

    feat: implement write function using stream api

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,26 @@
+import { open } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const write = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+  const fd = await open(filePath, "w+");
+  const fileStream = fd.createWriteStream();
+
+  output.write("Use Ctrl + C to stop stdin\n");
+  output.write("Enter some text:\n");
+  input.on("data", (chunk) => {
+    const data = chunk.toString();
+    fileStream.write(data);
+  });
+
+  input.on("end", () => {
+    fileStream.end();
+  });
 };
 
 await write();
