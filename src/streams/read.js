23f9da38ab15e9e25323c8adda323a28017d24d5
Sergey Masiuk <mayerror@gmail.com>
2024-01-29 22:10:22 +0300

    feat: implement read function using stream api

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,19 @@
+import { open } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { stdout } from "node:process";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const read = async () => {
-    // Write your code here 
+  const filePath = join(__dirname, "files", "fileToRead.txt");
+  const fd = await open(filePath);
+  const readable = fd.createReadStream();
+  readable.pipe(stdout);
+
+  readable.on("end", () => {
+    fd.close();
+  });
 };
 
 await read();
