da2ad1297ff7011e3f0d6798ece78f8d7841e566
Sergey Masiuk <mayerror@gmail.com>
2024-01-29 16:04:43 +0300

    feat: create basic structure for copy function

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,21 @@
+import { open, stat } from "node:fs/promises";
+import { dirname, join } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    const dirPathS = join(__dirname, "files");
+    const dirPathD = join(__dirname, "files_copy");
+
+    await stat(dirPathS);
+  } catch (error) {
+    // console.log(error);
+    if (error.code == "EEXIST" || error.code == "ENOENT") {
+      throw Error("FS operation failed");
+    }
+  }
 };
 
 await copy();
